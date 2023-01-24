31dd339db3122cbdc5a44515b2b3cf3f0733dc14
qwe2733227 <qwe2733227@naver.com>
2023-01-24 19:00:43 +0900

    위지위그 수정

--- ApiTest/wysiwyg/final02/src/main/java/com/kh/final02/TestController.java
+++ ApiTest/wysiwyg/final02/src/main/java/com/kh/final02/TestController.java
@@ -1,10 +1,26 @@
 package com.kh.final02;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.JsonObject;
 
 @Controller
 public class TestController {
@@ -21,5 +37,60 @@ public class TestController {
 		System.out.println("eiwinfin여기진입 ");
 		
 		return mv;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "fileupload.do")
+    public void communityImageUpload(HttpServletRequest req, HttpServletResponse resp, MultipartHttpServletRequest multiFile) throws Exception{
+		JsonObject jsonObject = new JsonObject();
+		PrintWriter printWriter = null;
+		OutputStream out = null;
+		MultipartFile file = multiFile.getFile("upload");
+		
+		if(file != null) {
+			if(file.getSize() >0 && StringUtils.isNotBlank(file.getName())) {
+				if(file.getContentType().toLowerCase().startsWith("image/")) {
+				    try{
+				    	 
+			            String fileName = file.getOriginalFilename();
+			            byte[] bytes = file.getBytes();
+			           
+			            String uploadPath = req.getSession().getServletContext().getRealPath("/resources/images/noticeimg"); //저장경로
+			            System.out.println("uploadPath:"+uploadPath);
+
+			            File uploadFile = new File(uploadPath);
+			            if(!uploadFile.exists()) {
+			            	uploadFile.mkdir();
+			            }
+			            String fileName2 = UUID.randomUUID().toString();
+			            uploadPath = uploadPath + "/" + fileName2 +fileName;
+			            
+			            out = new FileOutputStream(new File(uploadPath));
+			            out.write(bytes);
+			            
+			            printWriter = resp.getWriter();
+			            String fileUrl = req.getContextPath() + "/resources/images/noticeimg/" +fileName2 +fileName; //url경로
+			            System.out.println("fileUrl :" + fileUrl);
+			            JsonObject json = new JsonObject();
+			            json.addProperty("uploaded", 1);
+			            json.addProperty("fileName", fileName);
+			            json.addProperty("url", fileUrl);
+			            printWriter.print(json);
+			            System.out.println(json);
+			 
+			        }catch(IOException e){
+			            e.printStackTrace();
+			        } finally {
+			            if (out != null) {
+		                    out.close();
+		                }
+		                if (printWriter != null) {
+		                    printWriter.close();
+		                }
+			        }
+				}
+		}
+		
+	}
 	}
 }
--- ApiTest/wysiwyg/final02/src/main/webapp/WEB-INF/views/index/aaa.jsp
+++ ApiTest/wysiwyg/final02/src/main/webapp/WEB-INF/views/index/aaa.jsp
@@ -1,33 +1,26 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<!-- <script src="https://cdn.ckeditor.com/ckeditor5/32.0.0/classic/ckeditor.js"></script> -->
-
-<script type="text/javascript" src=".../resources/ckeditor_4.20.1_standard/ckeditor/ckeditor.js"></script>
-
+<script type="text/javascript" src="C:\Users\LSC\Documents\GitHub\Project_Swith\ApiTest\wysiwyg\final02\src\main\webapp\resources\ckeditor\ckeditor.js"></script>
 </head>
+
 <body>
-	
 	<div class="mb-3" style="width: 50%; margin: 0 auto;">
-		<label for="exampleFormControlTextarea1" class="form-label">News Content</label>
-		<testarea class="form-control" name="freeboard_content" id="ckeditor" rows="6"></testarea>
+		<label for="exampleFormControlTextarea1" class="form-label">NewsContent</label>
+		<textarea class="form-control " name="freeboard_content" id="ckeditor" rows="6"></textarea>
 	</div>
-	<script>
-		CKEDITOR.replace('ckeditor',{ //해당 이름으로 된 textarea에 에디터를 적용
-		width:'100%',
-		height:'400%',
-		filebrowserUploadUrl:"fileupload.do"  //컨트롤러랑 연결해 줄 코드이다.
-		});
-<%-- 	let editor;
-		ClassicEditor.create(document.querySelector('#textarea_ck_test'),{
-			ckfinder: {uploadUrl: '<%=request.getContextPath()%>/wisupload'}})
-		.then(newEditor => {editor = newEditor;}).catch(function (error) {
-	        console.error(error);
-	    }); --%>
+	
+	<script type="text/javascript">
+		CKEDITOR.replace('ckeditor', {//해당 이름으로 된 textarea에 에디터를 적용
+	        width:'100%',
+	        height:'400px',
+	        filebrowserUploadUrl:  "fileupload.do"
+	    });
 	</script>
 </body>
 </html>
