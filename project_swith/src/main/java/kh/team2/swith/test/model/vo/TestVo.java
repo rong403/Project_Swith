2526796b5e0feb403004b1009323ada375685477
rong403 <fhdrl4603@gmail.com>
2023-01-27 18:05:26 +0900

    lombok 적용
    getter
    setter
    toString

--- project_swith/src/main/java/kh/team2/swith/test/model/vo/TestVo.java
+++ project_swith/src/main/java/kh/team2/swith/test/model/vo/TestVo.java
@@ -1,29 +1,22 @@
 package kh.team2.swith.test.model.vo;
 
+import java.sql.Date;
+
 import org.springframework.stereotype.Component;
 
-@Component
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Getter
+@Setter
 public class TestVo {
 	private String id;
 	private String pwd;
 	
 	public TestVo() {
 		super();
-	}
-	@Override
-	public String toString() {
-		return "TestVo [id=" + id + ", pwd=" + pwd + "]";
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
 	}
 }
