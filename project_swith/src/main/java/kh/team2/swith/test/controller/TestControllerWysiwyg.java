4cdfec3723e21b9a6e343da2eeb5bce1cb76ba1f
qwe2733227 <qwe2733227@naver.com>
2023-01-30 21:37:24 +0900

    CKEditor API 업로드

---
+++ project_swith/src/main/java/kh/team2/swith/test/controller/TestControllerWysiwyg.java
@@ -0,0 +1,97 @@
+package kh.team2.swith.test.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.JsonObject;
+
+@Controller
+public class TestControllerWysiwyg {
+
+	@GetMapping("/aaa")
+	public String aaa() {
+		String result = null;
+		
+		return "/index/aaa";
+	}
+	
+	@PostMapping("/aaa")
+	public ModelAndView wef(MultipartHttpServletRequest files, ModelAndView mv) {
+		System.out.println("eiwinfin여기진입");
+		
+		return mv;
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "fileupload.do")
+    public void communityImageUpload(HttpServletRequest req, HttpServletResponse resp, MultipartHttpServletRequest multiFile) throws Exception{
+		JsonObject jsonObject = new JsonObject();
+		PrintWriter printWriter = null;
+		OutputStream out = null;
+		MultipartFile file = multiFile.getFile("upload");
+		
+		if(file != null) {
+			if(file.getSize() >0 && StringUtils.isNotBlank(file.getName())) {
+				if(file.getContentType().toLowerCase().startsWith("image/")) {
+				    try{
+				    	 
+			            String fileName = file.getOriginalFilename();
+			            byte[] bytes = file.getBytes();
+			           
+			            String uploadPath = req.getSession().getServletContext().getRealPath("/resources/images/noticeimg");
+			            System.out.println("uploadPath:"+uploadPath);
+
+			            File uploadFile = new File(uploadPath);
+			            if(!uploadFile.exists()) {
+			            	uploadFile.mkdirs();
+			            }
+			            String fileName2 = UUID.randomUUID().toString();
+			            uploadPath = uploadPath + "/" + fileName2 +fileName;
+			            
+			            out = new FileOutputStream(new File(uploadPath));
+			            out.write(bytes);
+			            
+			            printWriter = resp.getWriter();
+			            String fileUrl = req.getContextPath() + "/resources/images/noticeimg/" +fileName2 +fileName; 
+			            System.out.println("fileUrl :" + fileUrl);
+			            JsonObject json = new JsonObject();
+			            json.addProperty("uploaded", 1);
+			            json.addProperty("fileName", fileName);
+			            json.addProperty("url", fileUrl);
+			            printWriter.print(json);
+			            System.out.println(json);
+			 
+			        }catch(IOException e){
+			            e.printStackTrace();
+			        } finally {
+			            if (out != null) {
+		                    out.close();
+		                }
+		                if (printWriter != null) {
+		                    printWriter.close();
+		                }
+			        }
+				}
+		}
+		
+	}
+	}
+}
