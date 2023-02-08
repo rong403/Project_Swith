e54fa7d2e40008c6c7c5f2d45a42e8f511e2c722
qwe2733227 <qwe2733227@naver.com>
2023-02-08 17:58:29 +0900

    Board Dao, Service, Vo 복구

---
+++ project_swith/src/main/java/kh/team2/swith/board/model/service/BoardWriteService.java
@@ -0,0 +1,7 @@
+package kh.team2.swith.board.model.service;
+
+import kh.team2.swith.board.model.vo.BoardWrite;
+
+public interface BoardWriteService {
+	public int insertBoard(BoardWrite vo)  throws Exception;
+}
