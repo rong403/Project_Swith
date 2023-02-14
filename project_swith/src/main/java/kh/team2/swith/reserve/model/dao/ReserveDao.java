b74790d1dae4159e55ea42d1c251f9340a0cf9b6
HyeJng <hhjngmys@gmail.com>
2023-02-14 18:20:10 +0900

    reserve/ dao mapper 연결 추가

--- project_swith/src/main/java/kh/team2/swith/reserve/model/dao/ReserveDao.java
+++ project_swith/src/main/java/kh/team2/swith/reserve/model/dao/ReserveDao.java
@@ -18,17 +18,17 @@ public class ReserveDao {
 		return session.insert("reserve.insertReserve", vo);
 	}
 	public int updateReserve(ReserveInfo vo) {
-		return session.update("", vo);
+		return session.update("reserve.updateReserve", vo);
 	}
 	public int deleteReserve(int reserve_no) {
-		return session.delete("", reserve_no);
+		return session.delete("reserve.deleteReserve", reserve_no);
 	}
 	//TODO hhjng
 	public ReserveInfo selectReserve(int reserve_no) {
-		return session.selectOne("", reserve_no);
+		return session.selectOne("reserve.selectReserve", reserve_no);
 	}
 	public List<ReserveInfo> selectListReserve(){
-		return session.selectList("");
+		return session.selectList("reserve.selectListReserve");
 	}
 	//TODO hhjng
 	public int selectReserveCount() {
