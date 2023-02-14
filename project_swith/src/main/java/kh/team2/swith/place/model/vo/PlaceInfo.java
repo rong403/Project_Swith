502792c7125635c330c8e48f9b8612b3550ae975
homin0203 <kwonhomin317@gmail.com>
2023-02-14 20:39:30 +0900

    place,room / vo,mapper 수정
    ERD 수정으로 인한 수정 및 vo lombok 적용

--- project_swith/src/main/java/kh/team2/swith/place/model/vo/PlaceInfo.java
+++ project_swith/src/main/java/kh/team2/swith/place/model/vo/PlaceInfo.java
@@ -2,6 +2,13 @@ package kh.team2.swith.place.model.vo;
 
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 @Component
 public class PlaceInfo {
 //	"P_NO"	NUMBER		NOT NULL,
@@ -20,86 +27,4 @@ public class PlaceInfo {
 	private double p_x;
 	private double p_y;
 	private String area_code;
-	
-	public PlaceInfo() {
-		super();
-	}
-	
-	public PlaceInfo(int p_no, String p_name, String p_address, String p_info) {
-		super();
-		this.p_no = p_no;
-		this.p_name = p_name;
-		this.p_address = p_address;
-		this.p_info = p_info;
-	}
-
-	@Override
-	public String toString() {
-		return "PlaceInfo [p_no=" + p_no + ", p_name=" + p_name + ", p_address=" + p_address + ", p_info=" + p_info
-				+ ", p_phone=" + p_phone + ", p_x=" + p_x + ", p_y=" + p_y + ", area_code=" + area_code + "]";
-	}
-
-	public int getP_no() {
-		return p_no;
-	}
-
-	public void setP_no(int p_no) {
-		this.p_no = p_no;
-	}
-
-	public String getP_name() {
-		return p_name;
-	}
-
-	public void setP_name(String p_name) {
-		this.p_name = p_name;
-	}
-
-	public String getP_address() {
-		return p_address;
-	}
-
-	public void setP_address(String p_address) {
-		this.p_address = p_address;
-	}
-
-	public String getP_info() {
-		return p_info;
-	}
-
-	public void setP_info(String p_info) {
-		this.p_info = p_info;
-	}
-
-	public String getP_phone() {
-		return p_phone;
-	}
-
-	public void setP_phone(String p_phone) {
-		this.p_phone = p_phone;
-	}
-
-	public double getP_x() {
-		return p_x;
-	}
-
-	public void setP_x(double p_x) {
-		this.p_x = p_x;
-	}
-
-	public double getP_y() {
-		return p_y;
-	}
-
-	public void setP_y(double p_y) {
-		this.p_y = p_y;
-	}
-
-	public String getArea_code() {
-		return area_code;
-	}
-
-	public void setArea_code(String area_code) {
-		this.area_code = area_code;
-	}
 }
--- project_swith/src/main/java/kh/team2/swith/place/room/model/vo/RoomImg.java
+++ project_swith/src/main/java/kh/team2/swith/place/room/model/vo/RoomImg.java
@@ -4,20 +4,15 @@ import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @Component
 public class RoomImg {
-	private int room_img;
+	private int room_img_no;
 	private String room_img_origin;
 	private String room_img_save;
 	private String room_img_route;
-	private int room_no;
-	
-	@Override
-	public String toString() {
-		return "RoomImg [room_img=" + room_img + ", room_img_origin=" + room_img_origin + ", room_img_save="
-				+ room_img_save + ", room_img_route=" + room_img_route + ", room_no=" + room_no + "]";
-	}
 }
