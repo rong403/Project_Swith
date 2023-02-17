3d895ef93be0b68cc132abdd4f80853436bd51d5
homin0203 <kwonhomin317@gmail.com>
2023-02-17 23:57:47 +0900

    area / controller / ajax 시군구 조회
    미 업로드 파일 업로드

---
+++ project_swith/src/main/java/kh/team2/swith/area/controller/AreaController.java
@@ -0,0 +1,33 @@
+package kh.team2.swith.area.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import kh.team2.swith.area.model.service.AreaService;
+import kh.team2.swith.area.model.vo.Area;
+
+@Controller
+public class AreaController {
+
+	@Autowired
+	private AreaService service;
+	
+	@PostMapping("/sigungu.lo")
+	@ResponseBody
+	public String sigungulistAjax(@RequestParam("sido_name") String sido_name) throws Exception {
+		List<Area> volist = null;
+		if(!sido_name.equals("선택")) {
+			volist = service.selectSigunguList(sido_name);
+		}
+		return new Gson().toJson(volist);
+	}
+}
