30f4f201d6cdeafec85e3b0e15a0f36080740fd5
cho2zhh <cho2zhh@gmail.com>
2023-03-11 00:17:55 +0900

    채팅 mapper/ VO

---
+++ project_swith/src/main/java/kh/team2/swith/chat/model/vo/Chat.java
@@ -0,0 +1,27 @@
+package kh.team2.swith.chat.model.vo;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Component
+public class Chat {
+//	agr_number, chatting_content, chatting_time, agr_date, out_date, member_id, auth_code
+	private int study_no;
+	private String member_id;
+	private String chatting_content;
+	
+	private int agr_number;
+	private Date req_date;
+	private Date agr_date;
+	private Date out_date;
+	private int auth_code;
+	
+}
