7f609bac503c6c288857399ec341d90b587d4529
cho2zhh <cho2zhh@gmail.com>
2023-03-11 18:56:13 +0900

    chat service 어노테이션 오류 수정

--- project_swith/src/main/java/kh/team2/swith/chat/service/ChatService.java
+++ project_swith/src/main/java/kh/team2/swith/chat/service/ChatService.java
@@ -2,24 +2,21 @@ package kh.team2.swith.chat.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import kh.team2.swith.chat.model.vo.Chat;
 import kh.team2.swith.member.model.vo.Member;
 import kh.team2.swith.study.model.vo.Study;
 
-@Service
 public interface ChatService {
 	
 	// 채팅방 리스트
-	public List<Study> selectRoom(String member_id) throws Exception;
+	List<Study> selectRoom(String member_id) throws Exception;
 	// 채팅 참여자 정보
-	public List<Member> readMember(String study_no) throws Exception;
+	List<Member> readMember(String study_no) throws Exception;
 	// 채팅방 멤버수
-	public int memberCnt(String study_no) throws Exception;
+	int memberCnt(String study_no) throws Exception;
 	// 채팅 DB 등록
-	public int insertChatting(Chat vo) throws Exception;
+	int insertChatting(Chat vo) throws Exception;
 	// 채팅 내용 조회
-	public List<Chat> selectChatting(String study_no) throws Exception;
+	List<Chat> selectChatting(String study_no) throws Exception;
 
 }
